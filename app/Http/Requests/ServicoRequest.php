e86cedf48feb5d9482a99f57cb15b5b7066b8c22
JoaoGuilhermeMA <joomoreira36@gmail.com>
2025-04-14 16:43:05 -0300

    validação do form de serviço

--- app/Http/Requests/ServicoRequest.php
+++ app/Http/Requests/ServicoRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class ServicoRequest extends FormRequest
 {
@@ -22,7 +23,28 @@ class ServicoRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'nome' => ['required', 'min:2', 'max:255']
+            'nome' => ['required', 'min:2', 'max:255'],
+            'icone' => [
+                'required',
+                Rule::in(['twf-cleaning-1', 'twf-cleaning-2', 'twf-cleaning-3'])
+            ],
+            'posicao' => ['required', 'integer'],
+            'valor_minimo' => ['required', 'numeric'],
+            'quantidade_horas' => ['required', 'integer'],
+            'porcentagem' => ['required', 'integer'],
+            'valor_quarto' => ['required', 'numeric'],
+            'horas_quarto' => ['required', 'integer'],
+            'valor_sala' => ['required', 'numeric'],
+            'horas_sala' => ['required', 'integer'],
+            'valor_banheiro' => ['required', 'numeric'],
+            'horas_banheiro' => ['required', 'integer'],
+            'valor_cozinha' => ['required', 'numeric'],
+            'horas_cozinha' => ['required', 'integer'],
+            'valor_quintal' => ['required', 'numeric'],
+            'horas_quintal' => ['required', 'integer'],
+            'valor_outros' => ['required', 'numeric'],
+            'horas_outros' => ['required', 'integer'],
+
         ];
     }
 }
