63e682327e8e155c927b68ade2e3b75016e0f987
JoaoGuilhermeMA <joomoreira36@gmail.com>
2025-04-15 12:26:28 -0300

    protegendo as rotas da aplicação

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,25 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\ServicoController;
 use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {});
+Route::get('/', [LoginController::class, 'showLoginForm'])->name('login');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Route::resource('usuarios', UsuarioController::class);
+    Route::resource('usuarios', UsuarioController::class);
 
-// rotas para trabalhar com serviços
-Route::get('/servicos', [ServicoController::class, 'index'])->name('servico.index');
-Route::get('servicos/create', [ServicoController::class, 'create'])->name('servicos.create');
-Route::post('/servicos', [ServicoController::class, 'store'])->name('servicos.store');
-Route::get('/servicos/{servico}/edit', [ServicoController::class, 'edit'])->name('servicos.edit');
-Route::put('/servicos/{servico}', [ServicoController::class, 'update'])->name('servicos.update');
+    // rotas para trabalhar com serviços
+    Route::get('/servicos', [ServicoController::class, 'index'])->name('servico.index');
+    Route::get('servicos/create', [ServicoController::class, 'create'])->name('servicos.create');
+    Route::post('/servicos', [ServicoController::class, 'store'])->name('servicos.store');
+    Route::get('/servicos/{servico}/edit', [ServicoController::class, 'edit'])->name('servicos.edit');
+    Route::put('/servicos/{servico}', [ServicoController::class, 'update'])->name('servicos.update');
+});
